fd59854308f30b9aacd67221218fa794ed3fe8f4
Leandro Segovia <ldlsegovia@gmail.com>
2015-05-10 17:25:50 -0300

    feat(): add Organizer.define method
    to create an Organizer::Klass based on name param

--- lib/organizer.rb
+++ lib/organizer.rb
@@ -1,5 +1,22 @@
 require "organizer/version"
+require "organizer/error"
+require "active_support/all"
 
 module Organizer
-  # Your code goes here...
+  extend Error
+
+  def self.define(_organize_name)
+    klass = create_organizer_class(_organize_name)
+  end
+
+  private
+
+  def self.create_organizer_class(_organize_name)
+    class_name = _organize_name.to_s.classify
+    self.const_set(class_name, Class.new)
+
+  rescue
+    raise_error(:invalid_organizer_name)
+  end
+
 end
--- spec/organizer_spec.rb
+++ spec/organizer_spec.rb
@@ -1,11 +1,26 @@
 require 'spec_helper'
 
 describe Organizer do
+
   it 'has a version number' do
     expect(Organizer::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe "#define" do
+
+    it "creates a Organizer::Data class" do
+      subject.define("data")
+      expect(subject.const_defined?("Data")).to be_truthy
+    end
+
+    it "raises error with invalid organizer name" do
+      expect { subject.define("invalid*class<name") }.to raise_organizer_error(:invalid_organizer_name)
+    end
+
+    it "raises error with nil organizer name" do
+      expect { subject.define(nil) }.to raise_organizer_error(:invalid_organizer_name)
+    end
+
   end
+
 end
