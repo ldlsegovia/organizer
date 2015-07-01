e70a9fc0bbdafb7d94e4aa65a5c0e69c18d2760f
Leandro Segovia <ldlsegovia@gmail.com>
2015-06-30 21:41:18 -0300

    feat(GroupOperation): add initial value

--- lib/organizer/group_operation.rb
+++ lib/organizer/group_operation.rb
@@ -2,12 +2,15 @@ class Organizer::GroupOperation < Organizer::Operation
   include Organizer::Error
 
   attr_reader :group_name
+  attr_reader :initial_value
 
   # @param _definition [Proc] contains logic to generate the value for this operation
   # @param _name [Symbol] symbol to identify this particular operation
   # @param _group_name [Symbol] to identify group related with this operation
-  def initialize(_definition, _name, _group_name)
+  # @param _initial_value [Object]
+  def initialize(_definition, _name, _group_name, _initial_value = 0)
     @group_name = _group_name
+    @initial_value = _initial_value
     super(_definition, _name)
   end
 
@@ -19,7 +22,7 @@ class Organizer::GroupOperation < Organizer::Operation
   # @raise [Organizer::OperationException] :execute_over_organizer_group_items_only
   def execute(_group_item)
     raise_error(:execute_over_organizer_group_items_only) if !_group_item.is_a?(Organizer::GroupItem)
-    _group_item.define_attribute(self.name, 0)
+    _group_item.define_attribute(self.name, self.initial_value)
     _group_item.each do |item|
       result = definition.call(_group_item, item)
       _group_item.define_attribute(self.name, result)
--- spec/organizer/group_operation_spec.rb
+++ spec/organizer/group_operation_spec.rb
@@ -4,10 +4,11 @@ describe Organizer::GroupOperation do
   describe "#initialize" do
     it "creates a new Operation instance" do
       proc = Proc.new {}
-      o = Organizer::GroupOperation.new(proc, :my_operation, :my_group)
+      o = Organizer::GroupOperation.new(proc, :my_operation, :my_group, 666)
       expect(o.definition).to eq(proc)
       expect(o.name).to eq(:my_operation)
       expect(o.group_name).to eq(:my_group)
+      expect(o.initial_value).to eq(666)
     end
   end
 
@@ -22,12 +23,20 @@ describe Organizer::GroupOperation do
 
     it "sets operation result as new attribute into group item param" do
       proc = Proc.new do |group_item, item|
-        group_item.attrs_sum ||= 0
         group_item.attrs_sum + item.age
       end
 
       Organizer::GroupOperation.new(proc, :attrs_sum, :my_group).execute(@group_item)
       expect(@group_item.attrs_sum).to eq(322)
+    end
+
+    it "sets initial value" do
+      proc = Proc.new do |group_item, item|
+        group_item.attrs_sum + item.age
+      end
+
+      Organizer::GroupOperation.new(proc, :attrs_sum, :my_group, 344).execute(@group_item)
+      expect(@group_item.attrs_sum).to eq(666)
     end
   end
 end
