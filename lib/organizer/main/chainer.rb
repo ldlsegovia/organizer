000d1f39f478a221d188798497152d8715740e1e
Leandro Segovia <ldlsegovia@gmail.com>
2015-10-31 21:11:47 -0300

    refactor(Chainer): remove group from chained methods

--- lib/organizer/main/chainer.rb
+++ lib/organizer/main/chainer.rb
@@ -2,11 +2,13 @@ module Organizer
   class Chainer
     include Organizer::Error
 
+    attr_reader :group
+
     CHAINABLE_METHODS = [
       { name: :skip_default_filter, pattern: /\Askip_default_filters$/, scope: :collection },
       { name: :sort, pattern: /\Asort_by$/, scope: :collection },
       { name: :filter, pattern: /\Afilter_by$/, scope: :collection },
-      { name: :group, pattern: /\Agroup_by_\w+$/, scope: :group },
+      { name: :group, pattern: /\Agroup_by_\w+$/, scope: nil },
       { name: :sort_group, pattern: /\Asort_\w+_by$/, scope: :group },
       { name: :filter_group, pattern: /\Afilter_\w+_by$/, scope: :group }
     ]
@@ -21,12 +23,13 @@ module Organizer
 
     def initialize
       @chained_methods = []
+      @group = nil
     end
 
     def chain(_method_name, _args)
       method = build_method(_method_name, _args)
-      validate_method(method)
-      @chained_methods << method
+      validate_chaining(method)
+      method.group? ? @group = method : @chained_methods << method
       self
     end
 
@@ -38,12 +41,17 @@ module Organizer
 
     def build_method(_method_name, _args)
       definition = get_method_definition(_method_name)
-      group_name = group_name_from_method(definition, _method_name) if definition[:scope] == :group
+
+      group_name = if definition[:name] == :group
+                     _method_name.to_s.gsub("group_by_", "")
+                   elsif definition[:scope] == :group
+                     group_name_from_method_name(definition, _method_name)
+                   end
+
       Organizer::ChainedMethod.new(definition[:name], _args.flatten, group_name)
     end
 
-    def group_name_from_method(_definition, _method_name)
-      return _method_name.to_s.gsub("group_by_", "") if _definition[:name] == :group
+    def group_name_from_method_name(_definition, _method_name)
       parts = _method_name.to_s.split("_")
       parts.shift
       parts.pop
@@ -54,9 +62,9 @@ module Organizer
       CHAINABLE_METHODS.find { |method| _method_name =~ method[:pattern] }
     end
 
-    def validate_method(_method)
+    def validate_chaining(_method)
+      raise_error(:invalid_chaining) if _method.group? && group
       validate_uniqueness(_method, :skip_default_filter)
-      validate_uniqueness(_method, :group)
     end
 
     def validate_uniqueness(_method, _method_name)
--- lib/organizer/main/executor.rb
+++ lib/organizer/main/executor.rb
@@ -8,7 +8,7 @@ module Organizer
       load_default_filters_executor(executors, _definitions, _chainer.skip_default_filter_methods)
       load_filters_executor(executors, _definitions, _chainer.filter_methods)
       load_sort_items_executor(executors, _chainer.sort_methods)
-      load_groups_executor(executors, _definitions, _chainer.group_methods.first)
+      load_groups_executor(executors, _definitions, _chainer.group)
       load_group_operations_executor(executors, _definitions)
       load_group_filters_executor(executors, _definitions, _chainer.filter_group_methods)
       load_group_sort_items_executor(executors, _chainer.sort_group_methods)
